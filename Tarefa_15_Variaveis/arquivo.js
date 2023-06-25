65897d77dde59b976b6da35f6d293a0b6cc07b73
Giselellima <130230416+Giselellima@users.noreply.github.com>
2023-06-24 21:18:10 -0300

    Add files via upload

---
+++ Tarefa_15_Variaveis/arquivo.js
@@ -0,0 +1,15 @@
+var aviso = "Bem Vindo! Página Carregada com Sucesso!";
+
+alert("Bem Vindo! Página Carregada com Sucesso!")
+
+var x=8
+var y=3
+
+console.log (x + y) 
+document.write("A soma de x e y é: " +(8+3) + "<br>" + "<br>");//11
+console.log (x/y)
+document.write('A divisão de x / y é: ' + (x/y) + "<br>" + "<br>");
+console.log (x >= y)
+document.write("X é maior ou igual a Y " + (x>=y) + "<br>" + "<br>")
+console.log(x * y)
+document.write('A multiplicação de x * y é: ' + (x*y) )
